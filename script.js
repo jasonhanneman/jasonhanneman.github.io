85749d3e8270e4391de9a99f59e2dbcb83a958da
Jason Hanneman <jason.hanneman@gmail.com>
2015-11-05 21:02:57 -0600

    Initial site

---
+++ script.js
@@ -0,0 +1,19 @@
+$(function () {
+    html2canvas($("body"), {
+        onrendered: function (canvas) {
+            $(".blurdiv").append(canvas);
+            $("canvas").attr("id", "canvas");
+            stackBlurCanvasRGB(
+                'canvas',
+            0,
+            0,
+            $("canvas").width(),
+            $("canvas").height(),
+            70);
+        }
+    });
+    vv = setTimeout(function () {
+        $("header").show();
+        clearTimeout(vv);
+    }, 200);
+});
